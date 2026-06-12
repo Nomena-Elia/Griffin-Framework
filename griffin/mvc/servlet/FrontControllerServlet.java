49f378d87767a63522d238054f7ad2985c3a8c83
Nomena-coder <raobelinanomenjanahary@gmail.com>
2026-06-12 08:21:58 +0300

    Front servlet

---
+++ griffin/mvc/servlet/FrontControllerServlet.java
@@ -0,0 +1,28 @@
+package griffin.mvc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontControllerServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    public void processRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String url = req.getRequestURL().toString();
+        PrintWriter out = res.getWriter();
+        out.println(url);
+        out.close();
+    }
+}
